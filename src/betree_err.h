d95ff70c3c116c943509121a8f1f8183ff8c6163
JungHwan Jaden Kim <zh.kim@samsung.com>
2025-01-16 17:00:46 +0900

    add a missing function in header

--- src/betree_err.h
+++ src/betree_err.h
@@ -1,10 +1,10 @@
 #pragma once
 
+#include "arraylist.h"
+#include "hashtable.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include "arraylist.h"
-#include "hashtable.h"
 
 typedef uint64_t betree_sub_t;
 
@@ -30,24 +30,39 @@ struct report_err {
  */
 void betree_init_err(struct betree_err* betree);
 struct betree_err* betree_make_err();
-struct betree_err* betree_make_with_parameters_err(uint64_t lnode_max_cap, uint64_t min_partition_size);
+struct betree_err* betree_make_with_parameters_err(
+    uint64_t lnode_max_cap, uint64_t min_partition_size);
 
 bool betree_make_sub_ids(struct betree_err* tree);
 
-void betree_add_boolean_variable_err(struct betree_err* betree, const char* name, bool allow_undefined);
-void betree_add_integer_variable_err(struct betree_err* betree, const char* name, bool allow_undefined, int64_t min, int64_t max);
-void betree_add_float_variable_err(struct betree_err* betree, const char* name, bool allow_undefined, double min, double max);
-void betree_add_string_variable_err(struct betree_err* betree, const char* name, bool allow_undefined, size_t count);
-void betree_add_integer_list_variable_err(struct betree_err* betree, const char* name, bool allow_undefined, int64_t min, int64_t max);
-void betree_add_integer_enum_variable_err(struct betree_err* betree, const char* name, bool allow_undefined, size_t count);
-void betree_add_string_list_variable_err(struct betree_err* betree, const char* name, bool allow_undefined, size_t count);
-void betree_add_segments_variable_err(struct betree_err* betree, const char* name, bool allow_undefined);
-void betree_add_frequency_caps_variable_err(struct betree_err* betree, const char* name, bool allow_undefined);
+void betree_add_boolean_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined);
+void betree_add_integer_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined, int64_t min, int64_t max);
+void betree_add_float_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined, double min, double max);
+void betree_add_string_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined, size_t count);
+void betree_add_integer_list_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined, int64_t min, int64_t max);
+void betree_add_integer_enum_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined, size_t count);
+void betree_add_string_list_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined, size_t count);
+void betree_add_segments_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined);
+void betree_add_frequency_caps_variable_err(
+    struct betree_err* betree, const char* name, bool allow_undefined);
+struct betree_variable_definition betree_get_variable_definition_err(
+    struct betree_err* betree, size_t index);
 
 bool betree_change_boundaries_err(struct betree_err* tree, const char* expr);
 
-const struct betree_sub* betree_make_sub_err(
-    struct betree_err* tree, betree_sub_t id, size_t constant_count, const struct betree_constant** constants, const char* expr);
+const struct betree_sub* betree_make_sub_err(struct betree_err* tree,
+    betree_sub_t id,
+    size_t constant_count,
+    const struct betree_constant** constants,
+    const char* expr);
 bool betree_insert_sub_err(struct betree_err* tree, const struct betree_sub* sub);
 
 /*
@@ -57,14 +72,28 @@ bool betree_insert_sub_err(struct betree_err* tree, const struct betree_sub* sub
 struct betree_event* betree_make_event_err(const struct betree_err* betree);
 
 bool betree_insert_err(struct betree_err* tree, betree_sub_t id, const char* expr);
-bool betree_insert_with_constants_err(struct betree_err* tree, betree_sub_t id, size_t constant_count, const struct betree_constant** constants, const char* expr);
+bool betree_insert_with_constants_err(struct betree_err* tree,
+    betree_sub_t id,
+    size_t constant_count,
+    const struct betree_constant** constants,
+    const char* expr);
 
-bool betree_search_err(const struct betree_err* tree, const char* event_str, struct report_err* report);
-bool betree_search_ids_err(const struct betree_err* tree, const char* event_str, struct report_err* report, const uint64_t* ids, size_t sz);
-bool betree_search_with_event_err(const struct betree_err* betree, struct betree_event* event, struct report_err* report);
-bool betree_search_with_event_ids_err(const struct betree_err* betree, struct betree_event* event, struct report_err* report, const uint64_t* ids, size_t sz);
+bool betree_search_err(
+    const struct betree_err* tree, const char* event_str, struct report_err* report);
+bool betree_search_ids_err(const struct betree_err* tree,
+    const char* event_str,
+    struct report_err* report,
+    const uint64_t* ids,
+    size_t sz);
+bool betree_search_with_event_err(
+    const struct betree_err* betree, struct betree_event* event, struct report_err* report);
+bool betree_search_with_event_ids_err(const struct betree_err* betree,
+    struct betree_event* event,
+    struct report_err* report,
+    const uint64_t* ids,
+    size_t sz);
 
-//bool betree_delete(struct betree_err* betree, betree_sub_t id);
+// bool betree_delete(struct betree_err* betree, betree_sub_t id);
 
 struct report_err* make_report_err();
 void free_report_err(struct report_err* report);
